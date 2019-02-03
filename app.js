1cd97d9abfc9963fce221233d2b235a7bff6ed9e
Arshin Jain <arshin.jain@outlook.com>
2019-02-02 23:46:08 -0800

    Counter Component to state from prop

--- app.js
+++ app.js
@@ -20,13 +20,17 @@ const Player = (props) => (
 );
 
 // Counter Component
-const Counter = (props) => (
-  <div className="counter">
-    <button className="counter-action decrement"> - </button>
-    <span className="counter-score">{props.score}</span>
-    <button className="counter-action increment"> + </button>
-  </div>
-);
+class Counter extends React.Component {
+  render() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">{this.props.score}</span>
+        <button className="counter-action increment"> + </button>
+      </div>
+    );
+  }
+}
 
 // App Component
 const App = (props) => (
