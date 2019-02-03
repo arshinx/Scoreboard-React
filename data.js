727fa4cb6694204f2ff06a0d3a0b1f85b8d5185b
Arshin Jain <arshin.jain@outlook.com>
2019-02-02 23:38:27 -0800

    Add ID for keys

--- data.js
+++ data.js
@@ -1,17 +1,21 @@
 const players = [
   {
+    id: 1,
     name: "Guil",
     score: 50
   },
   {
+    id: 2,
     name: "Ashley",
     score: 55
   },
   {
+    id: 3,
     name: "Eddy",
     score: 40
   },
   {
+    id: 4,
     name: "Jessica",
     score: 65
   }
