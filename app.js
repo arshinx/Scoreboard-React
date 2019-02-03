92188efe9db36997418dff10d47d6e0d449191e3
Arshin Jain <arshin.jain@outlook.com>
2019-02-03 00:19:14 -0800

    Implement Counter State

--- app.js
+++ app.js
@@ -15,18 +15,32 @@ const Player = (props) => (
     <span className="player-name">
       {props.name}
     </span>
-    <Counter score={props.score}/>
+    <Counter />
   </div>
 );
 
 // Counter Component
 class Counter extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      score: 0
+    };
+  }
+
+  /*
+    state = {
+      score: 0
+    };
+  */
+
+  // Renders DOM
   render() {
     return (
       <div className="counter">
-        <button className="counter-action decrement"> - </button>
-        <span className="counter-score">{this.props.score}</span>
-        <button className="counter-action increment"> + </button>
+        <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
+        <span className="counter-score">{this.state.score}</span>
+        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
       </div>
     );
   }
@@ -41,7 +55,6 @@ const App = (props) => (
     { props.initialPlayers.map( player =>
       <Player
         name={player.name}
-        score={player.score}
         key = {player.id.toString()}
       />
     )}
