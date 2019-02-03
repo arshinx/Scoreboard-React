189b02be2d0e6e3adc5a54532e2461374e41a4df
Arshin Jain <arshin.jain@outlook.com>
2019-02-02 17:10:07 -0800

    JSX as a templating language

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const title = 'React Element Title!';
 const desc = 'Creating React node and rendering it into DOM';
 
+const myTitleID = 'main-title';
+const name = 'Guil'
+
 const header = (
   <header>
-    <h1>{ title }</h1>
-    <p>{ desc }</p> 
+    <h1 id={myTitleID}>{ name }'s First Element</h1>
+    <p>{ desc }</p>
   </header>
 );
 
