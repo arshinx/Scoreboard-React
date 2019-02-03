c7b4a4ab81b7df1083ae3199a1e3f64c96c0f370
Arshin Jain <arshin.jain@outlook.com>
2019-02-02 21:43:01 -0800

    Add Player Data

---
+++ data.js
@@ -0,0 +1,18 @@
+const players = [
+  {
+    name: "Guil",
+    score: 50
+  },
+  {
+    name: "Ashley",
+    score: 55
+  },
+  {
+    name: "Eddy",
+    score: 40
+  },
+  {
+    name: "Jessica",
+    score: 65
+  }
+];
